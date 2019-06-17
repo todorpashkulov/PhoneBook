6024e0e887acb82d5f42131e4c98f08aa91a402e
Blueshadows <blueshadows98@gmail.com>
2019-06-17 15:01:40 +0300

    -Changed phonebookUtills to PhoneBookCreationUtills -added phoneBookModificationUtills -PhoneBookCreationUtills fully functional

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
 
-       PhoneBook phoneBook=PhoneUtills.getPhoneUtillsInstance().getPhoneBookInstanceFromFile("phonebook.txt");
+      PhoneBook phoneBook= PhoneBookCreationUtills.getPhoneUtillsInstance().getPhoneBookInstanceFromFile("phonebook.txt");
         System.out.println(phoneBook.getPairList());
+
     }
 }
